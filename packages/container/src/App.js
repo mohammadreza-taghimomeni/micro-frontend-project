d41fae8d298c23ba77fe34dc02af2e6f7c82527f
Mohammad Reza Taghimomeni <30400627+mohammadreza-taghimomeni@users.noreply.github.com>
2023-01-02 13:40:58 +0330

    init container webpack

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { StylesProvider } from '@material-ui/core/styles';
+
+export default () => {
+  return (
+    <h1>
+      hi there
+    </h1>
+  );
+};
