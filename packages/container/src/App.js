8a5b59b7e74d140749543bf6d2d6bada3d8dc056
Mohammad Reza Taghimomeni <30400627+mohammadreza-taghimomeni@users.noreply.github.com>
2023-01-11 15:22:48 +0330

    Handling CSS in Microfrontends

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
-    <BrowserRouter>
-      <Header />
-      
-      <MarketingApp />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+
+        <MarketingApp />
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
